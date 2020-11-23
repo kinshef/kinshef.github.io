c0025919a266ed51ccab62c8f0ad2ce7e704eecd
ravas <z.kinshef@gmail.com>
2020-11-23 16:56:29 +0300

    dev

--- tests/ test_OZiTAG/scripts/js.js
+++ tests/ test_OZiTAG/scripts/js.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelectorAll('.header-nav__item, .header-navDop').forEach((el) => {
+    document.querySelectorAll('.header-nav__item, .header-navDop__item').forEach((el) => {
         if(el.querySelector('.header-navDop')){
             el.classList.add('header-dopUl');
         }
     })
+    document.querySelectorAll('.header-nav__item, .header-navDop__item').forEach((el) => {
+        el.addEventListener('click', (el) => {
+            // if(el.target.parentNode.classList.contains('active')){
+                debugger
+                console.log(el.target.parentNode)
+            //     el.target.parentNode.classList.remove('active');
+            // }else{
+            //     el.target.parentNode.classList.add('active');
+            // }
+        })
+    })
+    
     document.querySelector('.header-btsMobile').addEventListener('click', () => {
         document.querySelector('.section-header').classList.toggle('active');
         document.body.classList.toggle('modal-open');
